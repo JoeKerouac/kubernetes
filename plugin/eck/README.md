0c882ec42e21e97206363a4157f7c6e174c1ba3a
JoeKerouac <1213812243@qq.com>
2021-07-19 12:02:01 +0800

    更新eck

--- plugin/eck/README.md
+++ plugin/eck/README.md
@@ -1,9 +1,12 @@
 # 部署流程
 > 官方文档参考：https://www.elastic.co/cn/elastic-cloud-kubernetes
 
-- 部署ECK（定义在all-in-one.yaml中），注意将其中的 `docker.elastic.co` 替换为自己的仓库，同时将 `docker.elastic.co/eck/eck-operator:1.6.0`
+> 快速部署：直接指定deploy.sh即可，传入一个参数：namespace（要部署到K8S的哪个命名空间中）
+
+
+- 部署ECK（定义在all-in-one.yml中），注意将其中的 `docker.elastic.co` 替换为自己的仓库，同时将 `docker.elastic.co/eck/eck-operator:1.6.0`
   替换为自己的镜像（因为这个仓库在国内无法访问）；
-- 部署elasticsearch，执行 `kubectl apply -f elasticsearch.yaml` 部署，部署前注意 `elasticsearch.yaml` 中声明的注意事项；
+- 部署elasticsearch，执行 `kubectl apply -f elasticsearch.yml` 部署，部署前注意 `elasticsearch.yml` 中声明的注意事项；
 - 部署完成可以使用 `kubectl get es` （或者 `kubectl get elasticsearch`） 来查看elasticsearch的部署状态；
 - 使用 `PASSWORD=$(kubectl get secret eck-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')` 来获取es的密码；
 - 使用 `curl -u "elastic:$PASSWORD" -k "https://eck-es-http:9200"` 来请求es，注意将域名替换为ip或者自己映射的域名；
---
+++ plugin/eck/deploy.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -o nounset
+set -e
+
+
+PRG="$0"
+# -h表示判断文件是否是软链接
+# 下面这个用于获取真实路径
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# 这里获取到的就是当前的目录（如果是sh执行就是相对路径，如果是exec就是绝对路径）
+NOW_DIR=`dirname "$PRG"`
+
+
+
+# 如果指定目录不存在则创建
+mkdirIfAbsent() {
+	if [ ! -d $1 ]; then
+	echo "目录[$1]不存在，创建..."
+	mkdir -p $1
+	fi
+}
+
+
+
+
+NAMESPACE=$1
+DELOY_DIR=${NOW_DIR}/${NAMESPACE}
+
+echo "部署eck到${NAMESPACE}"
+
+mkdirIfAbsent ${DELOY_DIR}
+rm -rf ${DELOY_DIR}/*
+
+cp all-in-one.yml ${DELOY_DIR}/all-in-one.yml.${NAMESPACE}
+cp es.yml ${DELOY_DIR}/es.yml.${NAMESPACE}
+cp kibana.yml ${DELOY_DIR}/kibana.yml.${NAMESPACE}
+
+
+
+sed -i "s/\${NAMESPACE}/${NAMESPACE}/g" ${DELOY_DIR}/all-in-one.yml.${NAMESPACE}
+sed -i "s/\${NAMESPACE}/${NAMESPACE}/g" ${DELOY_DIR}/es.yml.${NAMESPACE}
+sed -i "s/\${NAMESPACE}/${NAMESPACE}/g" ${DELOY_DIR}/kibana.yml.${NAMESPACE}
+
+
+
+kubectl apply -f ${DELOY_DIR}/all-in-one.yml.${NAMESPACE}
+kubectl apply -f ${DELOY_DIR}/es.yml.${NAMESPACE}
+kubectl apply -f ${DELOY_DIR}/kibana.yml.${NAMESPACE}
