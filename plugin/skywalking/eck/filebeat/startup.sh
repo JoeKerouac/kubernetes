e58c03d7362e5605e6031c10dbc54ea07fc0cf33
JoeKerouac <1213812243@qq.com>
2021-08-02 10:39:57 +0800

    修改filebeat镜像构建，将setup步骤移动到启动脚本中去

--- plugin/skywalking/eck/filebeat/startup.sh
+++ plugin/skywalking/eck/filebeat/startup.sh
@@ -8,10 +8,6 @@ fi
 
 # 上边的脚本执行完毕后需要重新切换
 cd /filebeat/filebeat-7.13.3-linux-x86_64
-# 开启elasticsearch模块
-/filebeat/filebeat-7.13.3-linux-x86_64/filebeat modules enable elasticsearch
-# 启动前配置
-/filebeat/filebeat-7.13.3-linux-x86_64/filebeat setup
 
 # 前台启动，日志打印到标准输出中
 /filebeat/filebeat-7.13.3-linux-x86_64/filebeat -e
